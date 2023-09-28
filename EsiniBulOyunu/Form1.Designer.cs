a3eed8708402a25b349432390e0d884aaf379ad8
Ceren Kaya <cerenkaya1997ck@hotmail.com>
2023-09-28 17:03:54 +0300

    Gelistirildi..

--- EsiniBulOyunu/Form1.Designer.cs
+++ EsiniBulOyunu/Form1.Designer.cs
@@ -29,31 +29,187 @@
         private void InitializeComponent()
         {
             pnlKartlar = new Panel();
+            gboYeniOyun = new GroupBox();
+            label1 = new Label();
+            rb1 = new RadioButton();
+            rb2 = new RadioButton();
+            rb5 = new RadioButton();
+            rb4 = new RadioButton();
+            rb3 = new RadioButton();
+            btnOyunuBaslat = new Button();
+            menuStrip1 = new MenuStrip();
+            yeniOyunToolStripMenuItem = new ToolStripMenuItem();
+            btnYeniOyun = new ToolStripMenuItem();
+            gboYeniOyun.SuspendLayout();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // pnlKartlar
             // 
+            pnlKartlar.Anchor = AnchorStyles.None;
+            pnlKartlar.BackColor = Color.Transparent;
             pnlKartlar.ForeColor = SystemColors.ActiveCaption;
-            pnlKartlar.Location = new Point(12, 12);
+            pnlKartlar.Location = new Point(12, 27);
             pnlKartlar.Name = "pnlKartlar";
-            pnlKartlar.Size = new Size(600, 600);
+            pnlKartlar.Size = new Size(559, 459);
             pnlKartlar.TabIndex = 0;
+            // 
+            // gboYeniOyun
+            // 
+            gboYeniOyun.Anchor = AnchorStyles.None;
+            gboYeniOyun.BackColor = Color.Transparent;
+            gboYeniOyun.Controls.Add(label1);
+            gboYeniOyun.Controls.Add(rb1);
+            gboYeniOyun.Controls.Add(rb2);
+            gboYeniOyun.Controls.Add(rb5);
+            gboYeniOyun.Controls.Add(rb4);
+            gboYeniOyun.Controls.Add(rb3);
+            gboYeniOyun.Controls.Add(btnOyunuBaslat);
+            gboYeniOyun.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            gboYeniOyun.ForeColor = Color.Green;
+            gboYeniOyun.Location = new Point(131, 141);
+            gboYeniOyun.Name = "gboYeniOyun";
+            gboYeniOyun.Size = new Size(294, 250);
+            gboYeniOyun.TabIndex = 0;
+            gboYeniOyun.TabStop = false;
+            gboYeniOyun.Text = "Yeni Oyun";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(45, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(155, 28);
+            label1.TabIndex = 2;
+            label1.Text = "Zorluk Seviyesi";
+            // 
+            // rb1
+            // 
+            rb1.AutoSize = true;
+            rb1.Location = new Point(44, 67);
+            rb1.Name = "rb1";
+            rb1.Size = new Size(124, 32);
+            rb1.TabIndex = 1;
+            rb1.TabStop = true;
+            rb1.Text = "Çok Kolay";
+            rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            rb2.AutoSize = true;
+            rb2.Location = new Point(44, 92);
+            rb2.Name = "rb2";
+            rb2.Size = new Size(83, 32);
+            rb2.TabIndex = 1;
+            rb2.TabStop = true;
+            rb2.Text = "Kolay";
+            rb2.UseVisualStyleBackColor = true;
+            // 
+            // rb5
+            // 
+            rb5.AutoSize = true;
+            rb5.Location = new Point(44, 167);
+            rb5.Name = "rb5";
+            rb5.Size = new Size(103, 32);
+            rb5.TabIndex = 1;
+            rb5.TabStop = true;
+            rb5.Text = "Çok Zor";
+            rb5.UseVisualStyleBackColor = true;
+            // 
+            // rb4
+            // 
+            rb4.AutoSize = true;
+            rb4.Location = new Point(44, 142);
+            rb4.Name = "rb4";
+            rb4.Size = new Size(62, 32);
+            rb4.TabIndex = 1;
+            rb4.TabStop = true;
+            rb4.Text = "Zor";
+            rb4.UseVisualStyleBackColor = true;
+            // 
+            // rb3
+            // 
+            rb3.AutoSize = true;
+            rb3.Checked = true;
+            rb3.Location = new Point(44, 117);
+            rb3.Name = "rb3";
+            rb3.Size = new Size(73, 32);
+            rb3.TabIndex = 1;
+            rb3.TabStop = true;
+            rb3.Text = "Orta";
+            rb3.UseVisualStyleBackColor = true;
+            // 
+            // btnOyunuBaslat
+            // 
+            btnOyunuBaslat.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            btnOyunuBaslat.Location = new Point(45, 203);
+            btnOyunuBaslat.Name = "btnOyunuBaslat";
+            btnOyunuBaslat.Size = new Size(190, 40);
+            btnOyunuBaslat.TabIndex = 0;
+            btnOyunuBaslat.Text = "Oyunu Başlat";
+            btnOyunuBaslat.UseVisualStyleBackColor = true;
+            btnOyunuBaslat.Click += btnOyunuBaslat_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { yeniOyunToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(583, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // yeniOyunToolStripMenuItem
+            // 
+            yeniOyunToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { btnYeniOyun });
+            yeniOyunToolStripMenuItem.Name = "yeniOyunToolStripMenuItem";
+            yeniOyunToolStripMenuItem.Size = new Size(48, 20);
+            yeniOyunToolStripMenuItem.Text = "Oyun";
+            // 
+            // btnYeniOyun
+            // 
+            btnYeniOyun.Name = "btnYeniOyun";
+            btnYeniOyun.ShortcutKeys = Keys.F2;
+            btnYeniOyun.Size = new Size(147, 22);
+            btnYeniOyun.Text = "Yeni Oyun";
+            btnYeniOyun.Click += btnYeniOyun_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(627, 627);
+            BackgroundImage = Properties.Resources.MicrosoftTeams_image;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(583, 498);
+            Controls.Add(gboYeniOyun);
+            Controls.Add(menuStrip1);
             Controls.Add(pnlKartlar);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Eşini Bul Oyunu";
             TopMost = true;
+            gboYeniOyun.ResumeLayout(false);
+            gboYeniOyun.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Panel pnlKartlar;
+        private GroupBox gboYeniOyun;
+        private Button btnOyunuBaslat;
+        private Label label1;
+        private RadioButton rb1;
+        private RadioButton rb2;
+        private RadioButton rb5;
+        private RadioButton rb4;
+        private RadioButton rb3;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem yeniOyunToolStripMenuItem;
+        private ToolStripMenuItem btnYeniOyun;
     }
 }
--- EsiniBulOyunu/Form1.cs
+++ EsiniBulOyunu/Form1.cs
@@ -3,16 +3,18 @@ namespace EsiniBulOyunu
     public partial class Form1 : Form
     {
         Random rnd = new Random();
-        int boyut = 10; //satýr ve sütun sayýsý
+
+        int boyut = 6; //satýr ve sütun sayýsý
         List<string> resimler = new List<string>();
         List<string> kartlar = new List<string>();
+        List<PictureBox> aciklar = new List<PictureBox>();
+        int yokEdilenAdet = 0;
         public Form1()
         {
 
             ResimleriYukle();
-            InitializeComponent();
-            KartlariSec();
-            KartlariDiz();
+            InitializeComponent(); // bu öðelerin özelliklerini (örneðin, konumlarý, boyutlarý, metin içerikleri vb.) baþlatan ve ayarlayan bir otomatik üretim metodur.
+
 
         }
 
@@ -25,12 +27,12 @@ namespace EsiniBulOyunu
                 if (!kartlar.Contains(resim))
                     kartlar.Add(resim);
             }
-            kartlar.AddRange(kartlar);
+            kartlar.AddRange(kartlar); //AddRange() bir koleksiyonu diðer bir koleksiyona eklemek istediðinizde kullanýlýr.
             KartlarKaristir();
             //kariþtir.
         }
 
-        private void KartlarKaristir()
+        private void KartlarKaristir() //yer deðiþtir.
         {
             string yedek;
             int talihliIndeks;
@@ -47,11 +49,11 @@ namespace EsiniBulOyunu
 
         private void ResimleriYukle()
         {
-            DirectoryInfo klasor = new DirectoryInfo("image");
+            DirectoryInfo klasor = new DirectoryInfo("image"); // Burada DirectoryInfo sýnýfýný kullanarak klasörlerin oluþturulmasý, içeriklerinin listelenmesi ve diðer iþlemler gerçekleþtirilmektedir
             FileInfo[] dosyalar = klasor.GetFiles();
 
-            foreach (FileInfo dosya in dosyalar)
-                resimler.Add(dosya.Name);
+            foreach (FileInfo item in dosyalar)
+                resimler.Add(item.Name);
         }
 
 
@@ -76,8 +78,9 @@ namespace EsiniBulOyunu
                     resimKutusu.Size = new Size(gen, yuk);
                     resimKutusu.Left = x * (resimKutusu.Width + bosluk);
                     resimKutusu.Top = y * (resimKutusu.Height + bosluk);
-                    resimKutusu.ImageLocation = @"image\" + kartlar[i];
+                    resimKutusu.ImageLocation = "back.jpg";
                     resimKutusu.SizeMode = PictureBoxSizeMode.Zoom;
+                    resimKutusu.Click += ResimKutusu_Click;
                     pnlKartlar.Controls.Add(resimKutusu);
 
                     i++;
@@ -89,6 +92,106 @@ namespace EsiniBulOyunu
 
 
 
+        }
+        //sender týklanan resim kutsuunu taþýr
+        private void ResimKutusu_Click(object? sender, EventArgs e)
+        {
+            //önceden açýlanlarýn kapanmasý
+            if (aciklar.Count == 2)
+            {
+
+                AciklarKapat();
+            }
+            PictureBox tiklanan = (PictureBox)sender;
+            // eðer ayný karta ikinci defa týklandysa bir þey yapmadan çýk
+            if (aciklar.Count == 1 && aciklar[0] == tiklanan)
+                return;
+
+            int kartIndeks = (int)tiklanan.Tag;
+            string resim = kartlar[kartIndeks];
+            tiklanan.ImageLocation = "image\\" + resim;
+            aciklar.Add(tiklanan);
+            //2.Kartý açtýðýnda yapýlacak eþleþme kontrolü
+            if (aciklar.Count == 2 && aciklar[0].ImageLocation == aciklar[1].ImageLocation)
+            {
+                Application.DoEvents(); //2 resmi ayný anda göstrip sonra yok ediyor : arayüzün yenilenmesini saðlayarak gecikmeli yok et. 
+                AciklariGecikmeliYokEt();
+                AciklarKapat();
+                //oyun bitti mi ?
+                if (yokEdilenAdet == kartlar.Count)
+                {
+                    MessageBox.Show("Oyun Bitti!");
+                    OyunuSifirla();
+                }
+            }
+
+
+        }
+
+        private void OyunuSifirla()
+        {
+            pnlKartlar.Controls.Clear();
+            yokEdilenAdet = 0;
+            kartlar.Clear();
+            aciklar.Clear();
+            gboYeniOyun.Show();
+            pnlKartlar.BackColor = Color.Transparent;
+        }
+
+
+
+
+        private void AciklarKapat()
+        {
+            foreach (PictureBox kutu in aciklar)
+            {
+                kutu.ImageLocation = "back.jpg";
+            }
+            aciklar.Clear();
+
+        }
+
+        private void AciklariGecikmeliYokEt()
+        {
+            Thread.Sleep(500);
+
+            foreach (PictureBox kutu in aciklar)
+            {
+                pnlKartlar.Controls.Remove(kutu);
+                yokEdilenAdet++;
+            }
+
+        }
+
+        private void btnOyunuBaslat_Click(object sender, EventArgs e)
+        {
+            OyunuBaslat();
+        }
+
+        private void OyunuBaslat()
+        {
+            pnlKartlar.BackColor = Color.WhiteSmoke;
+            SeviyeyeKararVer();
+            gboYeniOyun.Hide();
+            KartlariSec();
+            KartlariDiz();
+
+
+        }
+
+        private void SeviyeyeKararVer()
+        {
+            if (rb1.Checked) boyut = 2;
+            else if (rb2.Checked) boyut = 4;
+            else if (rb3.Checked) boyut = 6;
+            else if (rb4.Checked) boyut = 8;
+            else boyut = 10;
+        }
+
+        private void btnYeniOyun_Click(object sender, EventArgs e)
+        {
+
+            OyunuSifirla();
         }
     }
 }
